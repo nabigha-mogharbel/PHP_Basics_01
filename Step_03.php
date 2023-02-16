faf7d0507566fce2dad939b337e92576faec5308
Nabigha Mogharbel <nabighamogharbel@gmail.com>
2023-02-16 12:27:12 +0200

    step 3

---
+++ Step_03.php
@@ -0,0 +1,10 @@
+<?php
+$name = 'Nabigha';
+$email = "nabighamogharbel@gmail.com";
+
+echo '"my name is "$name" and my email is "$email" ';
+$height=10;
+$width=8;
+$area=$height*$width;
+echo $area;
+?>
